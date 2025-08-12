f53cd060ede37425c793c8c9dc2b174ff3d6cbcd
renar3d <renar3d@gmail.com>
2025-08-12 15:39:26 +0200

    ✨ Ajout des formulaires (version 2)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,38 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [   
+                'id'=> 1,
+                'title' => 'Je suis une super question',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'rating' => 20,
+                'author' => ['name' => 'Jean Dupont', 'avatar' => 'https://randomuser.me/api/portraits/men/44.jpg'],
+                'nbrOfResponse' => 15,
+            ],
+            [   
+                'id'=> 2,
+                'title' => 'Je suis une super question',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'rating' => 0,
+                'author' => ['name' => 'Jane Rae', 'avatar' => 'https://randomuser.me/api/portraits/women/90.jpg'],
+                'nbrOfResponse' => 7,
+            ],
+            [   
+                'id'=> 3,
+                'title' => 'Je suis une super question',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'rating' => -15,
+                'author' => ['name' => 'John Doe', 'avatar' => 'https://randomuser.me/api/portraits/men/64.jpg'],
+                'nbrOfResponse' => 2,
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions
         ]);
     }
 }
